fce2df3bda1eeec61730618c8443e6a6b39fb01c
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-10-07 16:08:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int X =0 , Y= 0;
+    cin >> X >> Y;
+    if((X>0)&&(Y>0))
+        cout << 1;
+    else if((X>0)&&(Y<0))
+        cout << 4;
+    else if((X<0)&&(Y>0))
+        cout << 2;
+    else if((X<0)&&(Y<0))
+        cout << 3;
+
+return 0;
+
+
+}
