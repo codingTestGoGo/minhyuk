8af5522f3534b7dff8ef6022ce0bbcdcf7f09674
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-12-05 02:08:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41480 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,47 +1,41 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-using ll = long long int;
 
-int N, K;
-vector<pair<int,int>> vec;
-ll dp[100010];
-int visited[102][100010];
-
+int dp[101][100001];
 
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin >> N >> K;
-    vec.push_back({0,0});
-    for(int i =0; i<N;i++){
-        pair<int,int> newPair;
-        cin >> newPair.first >> newPair.second;
-        vec.push_back(newPair);
+
+    int N, W;
+    cin >> N >> W;
+
+    vector<pair<int,int>> vec;
+    int weight ,val;
+    for(int i =0;i<N;i++){
+        cin >> weight >> val;
+        vec.push_back({weight,val});
     }
 
     sort(vec.begin(),vec.end());
-    
-    for(int i =1; i<=N;i++){
-        // for(int t=0;t<N;t++){
-        //     if(i - vec[t].first >=0){
-        //         if(visited[i-vec[t].first][t] == 1){
-        //             visited[i][t] = 1;
-        //         }
-        //     }
-        // }
-        for(int t=0;t<=K;t++){
-            if(vec[i].first > t){
-                visited[i][t] = visited[i-1][t];
+
+
+    for(int i=1;i<=N;i++){
+        int tempWeight = vec[i-1].first;
+        int tempVal = vec[i-1].second;
+        for(int w = 0;w<=W;w++){
+            if(w-tempWeight >=0){
+                dp[i][w] = max(dp[i-1][w],dp[i-1][w-tempWeight] + tempVal);
             }else{
-                visited[i][t] = max(visited[i-1][t], visited[i-1][t-vec[i].first]+vec[i].second);
+                dp[i][w] = dp[i-1][w];
             }
+        }
     }
-    // for(int i=0;i<=K;i++){
-    //     cout << dp[i] << " ";
-    // }
-    // cout << "\n";
-    //다시는 냅색을 헷갈리지 말고 틀리지 말자... 이건 아니다.
-    }
-    cout << visited[N][K] << "\n";
+
+
+    cout << dp[N][W] << "\n";
+
+
+
 }
